7950a01450298839acc1629545be89b3b137673b
vrojak <florian_swienty@hotmail.de>
2019-05-08 17:35:06 +0200

    stuff is working yay

--- cma_testing/Start.py
+++ cma_testing/Start.py
@@ -7,32 +7,31 @@ import cma
 
 # START_VEL = np.array([[0,0,0]])
 # START_POS = np.array([[0,0,0]])
-GOAL_VEL = np.array([[0,0,0],[0,0,0]])
-GOAL_POS = np.array([[4,4,4],[-2,-2,-2]])
+GOAL_VEL = np.array([[0,0,0],[0,0,0],[2,2,2]])
+GOAL_POS = np.array([[4,4,4],[-2,-2,-2],[0,0,0]])
 
 TIMESTEP = 1
 NUM_AGENTS = GOAL_VEL.shape[0]
 NUM_TRAJ = 10
 NUM_DIM = GOAL_VEL.shape[1]
 
-num_opt_vars = NUM_AGENTS*NUM_TRAJ*NUM_DIM # the amount of variables to be optimized via cma
+MAX_JERK = 1
 
+num_opt_vars = NUM_AGENTS*NUM_TRAJ*NUM_DIM # the amount of variables to be optimized via cma
 error_calc = TrajectoryError.ErrorCalculator(TIMESTEP, NUM_TRAJ, GOAL_VEL, GOAL_POS)
 
-# options = cma.CMAOptions()
-# options.set('tolfun', 1e-3)
-es = cma.CMAEvolutionStrategy(num_opt_vars * [0], 0.5)
-es.opts.set('tolfun', 1e-1)
-es.optimize(error_calc.get_error)
-print("\n### es.result_pretty() #############################################")
-es.result_pretty()
-xbest = es.result[0]
-# print("\n### JERK TRAJECTORY RESULT #############################################")
-# print(xbest.reshape(NUM_AGENTS, NUM_TRAJ, NUM_DIM))
 
-err = error_calc.get_error(xbest)
+options = cma.CMAOptions()
+options.set('ftarget', 1e-1)
+options.set('bounds', [-MAX_JERK, MAX_JERK])
+# es = cma.CMAEvolutionStrategy(num_opt_vars * [0], 0.5, options)
+# es.opts.set('opt', value) # use this for chaning options while running
+# es.optimize(error_calc.get_error)
+# xbest = es.result[0]
+xbest = cma.fmin2(error_calc.get_error, num_opt_vars * [0], 0.5, options)[0]
+
 print("\n### FINAL ERROR #############################################")
-print(err)
+print(error_calc.get_error(xbest))
 print("\n### FINAL JERK TRAJECTORY #############################################")
 print(error_calc.jerk_traj)
 print("\n### FINAL ACC TRAJECTORY #############################################")
