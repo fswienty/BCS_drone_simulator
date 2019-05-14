05a1a7519161f04bb73215c0d20154ada47286e9
vrojak <florian_swienty@hotmail.de>
2019-05-14 19:29:39 +0200

    panda3d tut

--- panda3d/panda.py
+++ panda3d/panda.py
@@ -1,6 +1,9 @@
 from math import pi, sin, cos
 from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
+from direct.actor.Actor import Actor
+from direct.interval.IntervalGlobal import Sequence
+from panda3d.core import Point3
 
 class MyApp(ShowBase):
 
@@ -18,6 +21,34 @@ class MyApp(ShowBase):
         # Add the spinCameraTask procedure to the task manager.
         self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
 
+        # Load and transform the panda actor.
+        self.pandaActor = Actor("models/panda-model", {"walk": "models/panda-walk4"})
+        self.pandaActor.setScale(0.005, 0.005, 0.005)
+        self.pandaActor.reparentTo(self.render)
+        # Loop its animation.
+        self.pandaActor.loop("walk")
+
+        # Create the four lerp intervals needed for the panda to
+        # walk back and forth.
+        pandaPosInterval1 = self.pandaActor.posInterval(10,
+                                                        Point3(0, -3, 0),
+                                                        startPos=Point3(0, 3, 0))
+        pandaPosInterval2 = self.pandaActor.posInterval(10,
+                                                        Point3(0, 3, 0),
+                                                        startPos=Point3(0, -3, 0))
+        pandaHprInterval1 = self.pandaActor.hprInterval(3,
+                                                        Point3(180, 0, 0),
+                                                        startHpr=Point3(0, 0, 0))
+        pandaHprInterval2 = self.pandaActor.hprInterval(3,
+                                                        Point3(0, 0, 0),
+                                                        startHpr=Point3(180, 0, 0))
+        # Create and play the sequence that coordinates the intervals.
+        self.pandaPace = Sequence(  pandaPosInterval1,
+                                    pandaHprInterval1,
+                                    pandaPosInterval2,
+                                    pandaHprInterval2,
+                                    name="pandaPace")
+        self.pandaPace.loop()
 
     # Define a procedure to move the camera.
     def spinCameraTask(self, task):
