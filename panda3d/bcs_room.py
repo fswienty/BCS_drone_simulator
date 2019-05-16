6f105d60daa9286f99cf1ef88712a166e48326c9
vrojak <florian_swienty@hotmail.de>
2019-05-16 17:54:50 +0200

    added basic room model and lighting

---
+++ panda3d/bcs_room.py
@@ -0,0 +1,48 @@
+import sys,os
+from math import pi, sin, cos
+from direct.showbase.ShowBase import ShowBase
+from direct.task import Task
+#from direct.actor.Actor import Actor
+#from direct.interval.IntervalGlobal import Sequence
+#from panda3d.core import Point3 # pylint: disable=no-name-in-module
+from panda3d.core import Filename
+from panda3d.core import DirectionalLight
+
+class BcsTest(ShowBase):
+
+    def __init__(self):
+        ShowBase.__init__(self)
+
+        # Get the location of the 'py' file I'm running:
+        self.modelDir = os.path.abspath(sys.path[0])
+        # Convert that to panda's unix-style notation.
+        self.modelDir = Filename.from_os_specific(self.modelDir).getFullpath() + "/models"
+
+        self.addRoom()
+        self.addLights()
+
+        # Add the procedure to the task manager.
+        #self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
+        #self.taskMgr.add(self.spinLightTask, "SpinLightTask")
+
+    # Define a procedure to move the camera.
+    def spinCameraTask(self, task):
+        angleDegrees = task.time * 16.0
+        angleRadians = angleDegrees * (pi / 180.0)
+        self.camera.setPos(20 * sin(angleRadians), -20.0 * cos(angleRadians), 10)
+        self.camera.setHpr(angleDegrees, -30, 0)
+        return Task.cont
+
+    def addRoom(self):
+        self.roomModel = self.loader.loadModel(self.modelDir + "/room_test/room_test.egg")
+        self.roomModel.reparentTo(self.render)
+
+    def addLights(self):
+        for i in range(0,3):
+            dlight = DirectionalLight("light")
+            dlnp = self.render.attachNewNode(dlight) # directional light node path
+            dlnp.setHpr(120 * i, -30, 0)
+            self.render.setLight(dlnp)
+
+app = BcsTest()
+app.run()
--- panda3d/panda.py
+++ panda3d/panda.py
@@ -3,7 +3,7 @@ from direct.showbase.ShowBase import ShowBase
 from direct.task import Task
 from direct.actor.Actor import Actor
 from direct.interval.IntervalGlobal import Sequence
-from panda3d.core import Point3
+from panda3d.core import Point3 # pylint: disable=no-name-in-module
 
 class MyApp(ShowBase):
 
