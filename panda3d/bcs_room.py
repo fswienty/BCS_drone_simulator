0666820918da12dac72faec6fcd53187ab3a6594
vrojak <florian_swienty@hotmail.de>
2019-05-19 19:08:29 +0200

    camera controls work but still buggy

--- panda3d/bcs_room.py
+++ panda3d/bcs_room.py
@@ -9,7 +9,12 @@ from panda3d.core import Filename
 from panda3d.core import DirectionalLight
 from panda3d.core import AntialiasAttrib
 from panda3d.core import MouseButton
+from panda3d.core import KeyboardButton
 from panda3d.core import WindowProperties
+from panda3d.core import VBase4
+from panda3d.core import LVector3f
+from panda3d.core import LVecBase3f
+#from panda3d.core import CSDefault
 
 class BcsTest(ShowBase):
 
@@ -21,7 +26,6 @@ class BcsTest(ShowBase):
         # setup model directory
         self.modelDir = os.path.abspath(sys.path[0]) # Get the location of the 'py' file I'm running:
         self.modelDir = Filename.from_os_specific(self.modelDir).getFullpath() + "/models" # Convert that to panda's unix-style notation.
-
         # setup scene
         self.addRoom()
         self.addLights()
@@ -52,28 +56,58 @@ class BcsTest(ShowBase):
             dlnp.setHpr((120 * i) + 1, -30, 0)
             self.render.setLight(dlnp)
         dlight = DirectionalLight("light")
+        dlight.setColor(VBase4(0.2, 0.2, 0.2, 0.2))
         dlnp = self.render.attachNewNode(dlight) # directional light node path
         dlnp.setHpr(1, 30, 0)
         self.render.setLight(dlnp)
 
     def toggleCameraControl(self):
-        print("boop")
         if self.cameraControlActive:
+            self.cameraControlActive = False
+            props = WindowProperties()
+            props.setCursorHidden(False)
+            self.win.requestProperties(props)
             self.taskMgr.remove("CameraControlTask")
         else:
+            self.cameraControlActive = True
+            props = WindowProperties()
+            props.setCursorHidden(True)
+            self.win.requestProperties(props)
             self.taskMgr.add(self.cameraControlTask, "CameraControlTask")
 
     def cameraControlTask(self, task):
         mw = self.mouseWatcherNode
+        curPos = self.camera.getPos()
+        curHpr = self.camera.getHpr()
+        #forward = self.camera.getRelativeVector(self.render, LVecBase3f(0,0,1))
+        forward = self.camera.getQuat().getForward()
+        right = self.camera.getQuat().getRight()
+        up = self.camera.getQuat().getUp()
+        moveSpeed = .1
+        rotSpeed = 20
+
         if mw.hasMouse():
-            speed = 20
-            deltaX = speed * mw.getMouseX()
-            deltaY = speed * mw.getMouseY()
+            deltaX = rotSpeed * mw.getMouseX()
+            deltaY = rotSpeed * mw.getMouseY()
             props = self.win.getProperties()
             self.win.movePointer(0, int(props.getXSize() / 2), int(props.getYSize() / 2))
-            curHpr = self.camera.getHpr()
             self.camera.setHpr(curHpr.getX() - deltaX, curHpr.getY() + deltaY, 0)
-        
+
+        deltaPos = LVector3f(0, 0, 0)
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('w','utf-8'))):
+            deltaPos += forward * moveSpeed
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('s','utf-8'))):
+            deltaPos -= forward * moveSpeed
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('d','utf-8'))):
+            deltaPos += right * moveSpeed
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('a','utf-8'))):
+            deltaPos -= right * moveSpeed
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('q','utf-8'))):
+            deltaPos += up * moveSpeed
+        if mw.isButtonDown(KeyboardButton.asciiKey(bytes('e','utf-8'))):
+            deltaPos -= up * moveSpeed
+        self.camera.setPos(curPos + deltaPos)
+
         return Task.cont
         
         
