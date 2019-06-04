e0e619d3f4bc1fa548a7d638c130107b35872977
Florian <florian_swienty@hotmail.de>
2019-06-04 16:47:12 +0200

    added autonomous sequence

--- lighthouse_testing/transformation_matrix.py
+++ lighthouse_testing/transformation_matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+# pylint: disable=no-name-in-module
 from panda3d.core import Vec3
 
 class Matrix:
@@ -20,3 +21,6 @@ print("[1, 1, 0]->", mat.transform(Vec3(1,1,0)))
 print("[1, 0, 1]->", mat.transform(Vec3(1,0,1)))
 print("[0, 1, 1]->", mat.transform(Vec3(0,1,1)))
 print("[1, 1, 1]->", mat.transform(Vec3(1,1,1)))
+
+print("center floor->", mat.transform(Vec3(0.5,0.5,0)))
+print("center air->", mat.transform(Vec3(0.5,0.5,1)))
