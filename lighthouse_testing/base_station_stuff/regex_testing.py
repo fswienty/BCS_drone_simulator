26eeab1e1074a853ead8cffd92f818d79b634ecc
vrojak <florian_swienty@hotmail.de>
2019-06-06 14:12:04 +0200

    regex stuff

--- lighthouse_testing/base_station_stuff/regex_testing.py
+++ lighthouse_testing/base_station_stuff/regex_testing.py
@@ -6,8 +6,13 @@ import re
 with open(os.path.join(sys.path[0], "bs_position.txt"), encoding="utf-8") as f:
     text = f.read()
 
-match = re.search(r'-?\d', "drone45434")
-print(match.value)
+match = re.search(r'-?\d', "drone45-434")
+if match:
+    print(match.group())
+
+match = re.findall(r'\+?-?\d+', "drone45-43+4654-6567+8")
+print(match)
+
 
 
 # bs1 = LighthouseBsGeometry()
