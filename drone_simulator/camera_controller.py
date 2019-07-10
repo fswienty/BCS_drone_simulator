acbdb5cf3b39a77c4a8514d305984dd774fa5720
Florian <florian_swienty@hotmail.de>
2019-07-10 15:06:07 +0200

    refactoring

--- drone_simulator/camera_controller.py
+++ drone_simulator/camera_controller.py
@@ -9,38 +9,42 @@ class CameraController(DirectObject.DirectObject):
     def __init__(self, base):
         self.base = base
         self.base.disableMouse()
+
         self.camera = base.camera
         self.cameraControlActive = False
-        self.accept("mouse3", self.activateCameraControl)
-        self.accept("mouse3-up", self.deactivateCameraControl)
         self.camera.setPos(-5, 0, 2)
         self.camera.lookAt(0, 0, 1)
         base.camLens.setFov(90)
         base.camLens.setNear(.1)
-        self.setup = True
+
+        self.anchorX = 0
+        self.anchorY = 0
+        self.setAnchor = True
+
+        self.accept("mouse3", self.activateCameraControl)
+        self.accept("mouse3-up", self.deactivateCameraControl)
 
 
     def activateCameraControl(self):
         props = WindowProperties()
         self.cameraControlActive = True
-        # props.setCursorHidden(True)
+        props.setCursorHidden(True)
         self.base.win.requestProperties(props)
         windowSizeX = self.base.win.getProperties().getXSize()
         windowSizeY = self.base.win.getProperties().getYSize()
-        self.setup = True
+        self.setAnchor = True
         self.base.taskMgr.add(self.cameraControlTask, "CameraControlTask", extraArgs=[windowSizeX, windowSizeY], appendTask=True)
 
 
     def deactivateCameraControl(self):
         props = WindowProperties()
         self.cameraControlActive = False
-        # props.setCursorHidden(False)
+        props.setCursorHidden(False)
         self.base.win.requestProperties(props)
         self.base.taskMgr.remove("CameraControlTask")
 
 
-    anchorX = 0
-    anchorY = 0
+
     def cameraControlTask(self, windowSizeX, windowSizeY, task):
 
         mw = self.base.mouseWatcherNode
@@ -52,13 +56,12 @@ class CameraController(DirectObject.DirectObject):
         moveSpeed = .05
         rotSpeed = 20
 
-        # get initial mouse position
-        if self.setup:
+        # get initial mouse position, this runs only once on the first frame after the task is added to the taskmanager
+        if self.setAnchor:
             print("setup")
             self.anchorX = mw.getMouseX()
             self.anchorY = mw.getMouseY()
-            self.setup = False
-            #return task.cont
+            self.setAnchor = False
 
         # update rotation
         if mw.hasMouse():
