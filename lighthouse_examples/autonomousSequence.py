d00455194bd3ad080d4ca4cd654b6a8b364daca8
Florian <florian_swienty@hotmail.de>
2019-10-14 16:59:03 +0200

    modified autonomous sequence for testing

--- lighthouse_examples/autonomousSequence.py
+++ lighthouse_examples/autonomousSequence.py
@@ -43,14 +43,15 @@ from cflib.crazyflie.syncLogger import SyncLogger
 from cflib.crazyflie.commander import Commander
 
 # URI to the Crazyflie to connect to
-uri = 'radio://0/80/2M/E7E7E7E7E4'
+uri = 'radio://0/80/2M/E7E7E7E7E3'
 
 # Change the sequence according to your setup
 # x    y    z  YAW
 sequence = [
-    (0, 0, 0, 0),
-    (0, 0, .2, 0),
-    (0, 0, 0, 0),
+    (0.4, 0.35, 0, 0),
+    (1, 0, 0, 0),
+    (1, 1, 0, 0),
+    (0.4, 0.35, -0.5, 0)
 ]
 
 yawstuff = [
--- lighthouse_examples/print_position.py
+++ lighthouse_examples/print_position.py
@@ -76,10 +76,11 @@ def start_position_printing(scf):
 
 if __name__ == '__main__':
     # URI to the Crazyflie to connect to
-    uri = 'radio://0/80/2M/E7E7E7E7E1'
+    uri = 'radio://0/80/2M/E7E7E7E7E3'
 
     # Initialize the low-level drivers (don't list the debug drivers)
     cflib.crtp.init_drivers(enable_debug_driver=False)
     with SyncCrazyflie(uri, cf=Crazyflie(rw_cache='./cache')) as scf:
         reset_estimator(scf)
         start_position_printing(scf)
+        time.sleep(10)
